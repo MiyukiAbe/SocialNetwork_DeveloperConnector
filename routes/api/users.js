925f85a44d41d368c35e7716ecc6bb1e03af480e
Miyuki Abe <miyuki0809@gmail.com>
2019-06-25 18:38:26 -0400

    feat: created api route files and finished ch2

---
+++ routes/api/users.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+//@route GET api/users
+//@desc Test route
+//@access Public (to access this api, Token is not required)
+router.get('/', (req, res) => {
+  res.send('User Route');
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-const express = require("express");
-const connectDB = require("./config/db");
+const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
 
 //Connect Database
 connectDB();
 
-app.get("/", (req, res) => {
-  res.send("API is running");
+app.get('/', (req, res) => {
+  res.send('API is running');
 });
+
+//define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 //process.env will look for an environment variable called "Port" to use when we deploy to Heroku that's gonna get
 //the port number and locally, I am using 5000
